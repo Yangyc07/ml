eb7e2a962a4b30eb9041943e9afd33b531df37f8
Yangyc07 <747378285@qq.com>
2020-07-12 20:19:19 +0800

    代价

--- ex1/computeCostMulti.m
+++ ex1/computeCostMulti.m
@@ -13,10 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
-
+prediction = X * theta;
+J = 1 / (2 * m) * sum((prediction - y).^2);
 % =========================================================================
 
 end
